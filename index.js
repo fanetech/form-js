75c464a5691cb2afea7130071925b2b061f250c6
FIRST_NAME LAST_NAME <louguefranck.20@gmail.com>
2022-06-21 19:53:51 +0000

    fist commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let error = document.querySelector(".error")
+let success = document.querySelector(".success")
+let name = document.getElementById("name")
+let email = document.getElementById("email")
+
+
+const validatorForm = () =>{
+addEventListener(onclick, (e) =>{
+        e.preventDefault()
+    })
+}
+
+const handleClick = () =>{
+    let btn = document.getElementById("btn")
+    btn.addEventListener(onclick, (e) =>{
+        e.preventDefault()
+    })
+
+
+}
