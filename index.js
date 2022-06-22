959b8d9107dc65f96640d175e3e06a832ffa4e2c
FIRST_NAME LAST_NAME <louguefranck.20@gmail.com>
2022-06-22 15:10:13 +0000

    handling form validity and design css

--- index.js
+++ index.js
@@ -2,12 +2,37 @@ let error = document.querySelector(".error")
 let success = document.querySelector(".success")
 let name = document.getElementById("name")
 let email = document.getElementById("email")
+const form = document.getElementById("formSubmit")
+const btn = document.getElementById("btn")
 
 
-const validatorForm = () =>{
-addEventListener(onclick, (e) =>{
-        e.preventDefault()
-    })
+const validatorForm = (e) =>{
+    success.innerHTML = "" 
+    error.innerHTML = "" 
+    success.style.display='none'
+    error.style.display='none'
+    e.preventDefault()
+    if(!email.value || !name.value){
+        if(!email.value && !name.value){
+        error.innerHTML = "Veuillez renseigner tous les champs" 
+        error.style.display='block'
+        }
+        if(!email.value){
+            error.innerHTML = "Veuillez renseigner votre email" 
+            error.style.display='block'
+    
+        }
+        if(!name.value){
+            error.innerHTML = "Veuillez renseigner votre nom" 
+            error.style.display='block'
+    
+        }
+    }
+    else{
+        success.innerHTML = "Information valide" 
+        success.style.display='block'
+    } 
+
 }
 
 const handleClick = () =>{
@@ -18,3 +43,6 @@ const handleClick = () =>{
 
 
 }
+
+form.onsubmit = validatorForm
+btn.onclick =handleClick
